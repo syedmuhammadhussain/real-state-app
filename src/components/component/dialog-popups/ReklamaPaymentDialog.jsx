50df89547eb43557f9aa711fd58a2560b4cde2ce
syedmuhammadhussain <syedmuhammadhussain6@gmail.com>
2025-10-08 23:59:25 +0500

    add sorted orders

--- src/components/component/dialog-popups/ReklamaPaymentDialog.jsx
+++ src/components/component/dialog-popups/ReklamaPaymentDialog.jsx
@@ -209,7 +209,7 @@ export default function ReklamaPaymentDialog({ isOpen, setIsOpen, data }) {
                     </tr>
                   </thead>
                   <tbody>
-                    {positions?.map((item) => (
+                    {sortedPositions?.map((item) => (
                       <tr key={item.id} className="border-t">
                         <td className="px-4 py-2 border text-center">
                           {item.order}
